3a195dc745e4569d10ac7b1fb4e5fe651586c4c9
LuisRibeiro2903 <luisgithubribeiro@gmail.com>
2024-02-13 23:14:18 +0000

    Terminado

--- tpc1/README.md
+++ tpc1/README.md
@@ -1,10 +1,16 @@
 # TPC1
 
-* Numero Stor: 96_4
-
+**Título** Análise de dados em csv
+**Autor** Luís de Sá Ribeiro, A100608
 
 ## Objetivos
 
 * Lista ordenada alfabeticamente das modalidades desportivas;
 * Percentagens de atletas aptos e inaptos para a prática desportiva;
 * Distribuição de atletas por escalão etário (escalão = intervalo de 5 anos): ... [30-34], [35-39], ...
+
+## Resolução:
+
+Para satisfazer todos os objetivos impostos, foi criada uma classe `EMD_DTO`. Esta classe contém atributos respetivos às colunas do ficheiro csv mas não todas, apenas aquelas necessárias à resolução destes objetivos.
+
+De seguida, lê-se o ficheiro através do `stdin`, ou seja, é esperado que o *standard input* esteja redirecionado para o ficheiro, usando por exemplo o comando: `python3 tpc1.py < emd.csv`
--- tpc1/tpc1.py
+++ tpc1/tpc1.py
@@ -1,8 +1,8 @@
 import sys
 
 class EMD_DTO:
-
     #_id,nome/primeiro,nome/último,idade,modalidade,resultado    
+
     def __init__(self,
                  _id: str,
                  first_name: str,
@@ -10,11 +10,11 @@ class EMD_DTO:
                  age: int,
                  category: str,
                  result: bool):
-        self.id = _id,
-        self.first_name = first_name,
-        self.last_name = last_name,
-        self.age = age,
-        self.category = category,
+        self.id = _id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.category = category
         self.result = result
 
     
@@ -25,7 +25,7 @@ def populate_emd () -> list[EMD_DTO]:
         emd.append(parseLine(line.strip().split(',')))
     return emd
 
-def parseLine (values : list[str]):
+def parseLine (values: list[str]) -> EMD_DTO:
     return EMD_DTO(values[0],
                    values[3],
                    values[4],
@@ -33,21 +33,44 @@ def parseLine (values : list[str]):
                    values[8],
                    values[12] == 'true')
 
+def write_csv(file_name: str, data: list[list[str]]):
+    with open(file_name, 'w', encoding='utf-8') as file:
+        for row in data:
+            file.write(','.join(row) + '\n')
 
-def category_csv(emd : list[EMD_DTO]):
+def category_csv(emd: list[EMD_DTO]):
     data = [['modalidade']]
-    data.append(sorted(set([e.category for e in emd])))
-    print(data)
+    categories = sorted(set([e.category for e in emd]))  # Get unique categories and sort them
+    for category in categories:
+        data.append([category])  # Append each category as a separate list. This is done to use the generic "write_csv" function
+    write_csv('modalidades_ordenadas.csv', data)
 
-def aptos_csv(aptos, entradas):
-    percentagem_aptos = (aptos / entradas) * 100
-    print("Atletas aptos: " + str(percentagem_aptos) + "%")
-    print("Atletas inaptos: " + str(100 - percentagem_aptos) + "%")
+def apt_csv(emd: list[EMD_DTO]):
+    data = [['aptos,inaptos']]
+    total = float(len(emd))
+    total_apt = len(['dummy' for e in emd if e.result])
+    apt_percentage = total_apt / total * 100
+    inapt_percentage = 100 - apt_percentage
+    data.append([str(apt_percentage), str(inapt_percentage)])
+    write_csv('aptos_inaptos_percentagem.csv', data)
+
+def age_group_csv(emd: list[EMD_DTO], delta=5):
+    data = [['_id','nome/primeiro','nome/último','idade','escalão etário']]
+    ordered_by_age = sorted(emd, key=lambda x: x.age)
+    for e in ordered_by_age:
+        lower_bound = (e.age // delta) * delta
+        upper_bound = lower_bound + (delta - 1)
+        interval = f'[{lower_bound}-{upper_bound}]' 
+        data.append([e.id,e.first_name,e.last_name,str(e.age),interval])
+    write_csv('escalões_etários.csv', data)
+
 
 
 def main ():
     emd = populate_emd()
     category_csv(emd)
+    apt_csv(emd)
+    age_group_csv(emd)
 
 if __name__ == '__main__':
     main()
